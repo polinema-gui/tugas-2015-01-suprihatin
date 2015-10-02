095a11cf9cdd897d96750a0335c6690c68a2bbc9
suprihatin <atin0809@gmail.com>
2015-10-02 17:20:49 +0700

    tugas1

---
+++ tugas1/tugas1/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ubahButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ubahButton
+        '
+        Me.ubahButton.Location = New System.Drawing.Point(197, 195)
+        Me.ubahButton.Name = "ubahButton"
+        Me.ubahButton.Size = New System.Drawing.Size(64, 29)
+        Me.ubahButton.TabIndex = 0
+        Me.ubahButton.Text = "Ubah"
+        Me.ubahButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ubahButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ubahButton As System.Windows.Forms.Button
+
+End Class
---
+++ tugas1/tugas1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub ubahButton_Click(sender As Object, e As EventArgs) Handles ubahButton.Click
+        If Me.BackColor = Color.Yellow Then
+            Me.BackColor = Color.Blue
+        Else
+            Me.BackColor = Color.Yellow
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Yellow
+    End Sub
+End Class
